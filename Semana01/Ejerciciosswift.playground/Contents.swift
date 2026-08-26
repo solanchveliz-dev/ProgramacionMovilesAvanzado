512539e7e9651c2c414c6a76619092351c32df65
Naomi Solanch Veliz Pie <solanch.veliz@tecsup.edu.pe>
2026-08-26 09:48:12 -0500

    feat: resolver calculo de monto base e igv

--- Semana01/Ejerciciosswift.playground/Contents.swift
+++ Semana01/Ejerciciosswift.playground/Contents.swift
@@ -1,3 +1,42 @@
-import Cocoa
+import Foundation
 
-var greeting = "Hello, playground"
+let igv = 0.18
+
+func CalcularDeacuento(cantidad: Int, subtotal: Double) -> Double {
+    if cantidad > 10 {
+        return subtotal * 0.15
+    } else if cantidad > 5 {
+        return subtotal * 0.10
+    } else {
+        return 0.0
+    }
+}
+
+print("ingrese el nombre del producto")
+let producto = readLine() ?? ""
+
+print("ingrese el precio del producto")
+let precio = Double(readLine() ?? "0") ?? 0.0
+
+print("ingrese la cantidad de productos")
+let cantidad = Int(readLine() ?? "0") ?? 0
+
+let subtotal = Double(cantidad) * precio
+let descuento = CalcularDeacuento(cantidad: cantidad, subtotal: subtotal)
+
+// hallar el monto base: subtotal - descuento
+let total = subtotal - descuento
+
+// hallar el igv: monto base * igv
+let igvtotal = total * igv
+
+// total: monto base + igv
+let totalfinal = total + igvtotal
+
+//hallar el montobase: subtotal - descuento
+// hallar el igv el montobase*igv
+//total :montobase + igv
+// primer commmit
+//mostrar los datos
+//segundo commit
+
