d24f5bcc537a510d9b6aa6842d7a69ad08eb3699
Naomi Veliz <solanch.veliz@tecsup.edu.pe>
2026-08-31 23:47:28 -0500

    feat: agregar selección del plan de pago

--- Semana02/plandepago.playground/Contents.swift
+++ Semana02/plandepago.playground/Contents.swift
@@ -16,7 +16,30 @@ let montoCompra = precio * Double(cantidad)
 
 print("-------------------------")
 print("Producto: \(producto)")
-print("Precio: S/ \(precio)")
+print("Precio unitario: S/ \(precio)")
 print("Cantidad: \(cantidad)")
-print("Monto total: S/ \(montoCompra)")
+print("Monto total de la compra: S/ \(montoCompra)")
 print("-------------------------")
+print("Seleccione el plan de pago:")
+print("1. 6 meses")
+print("2. 12 meses")
+print("3. 24 meses")
+
+let opcion = Int(readLine() ?? "") ?? 0
+
+var meses = 0
+
+if opcion == 1 {
+    meses = 6
+} else if opcion == 2 {
+    meses = 12
+} else if opcion == 3 {
+    meses = 24
+} else {
+    print("Opción no válida")
+}
+
+if meses > 0 {
+    print("-------------------------")
+    print("Plan seleccionado: \(meses) meses")
+}
