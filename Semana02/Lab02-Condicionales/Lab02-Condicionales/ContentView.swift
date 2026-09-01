a6a82593c0fb5ac8fdc81bd57f23d63661982825
Naomi Veliz <solanch.veliz@tecsup.edu.pe>
2026-09-01 17:32:17 -0500

    feat: Crear proyecto Lab02 y Playground en Semana02

---
+++ Semana02/Lab02-Condicionales/Lab02-Condicionales/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab02-Condicionales
+//
+//  Created by Naomi Solanch Veliz Pie on 1/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
