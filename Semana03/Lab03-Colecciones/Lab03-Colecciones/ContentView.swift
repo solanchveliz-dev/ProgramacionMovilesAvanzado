0ea99c966426a2e0c621ad220e55e43be232f07a
Naomi Veliz <solanch.veliz@tecsup.edu.pe>
2026-09-02 09:28:39 -0500

    feat: Crear proyecto Lab03 y Playground en Semana03

---
+++ Semana03/Lab03-Colecciones/Lab03-Colecciones/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab03-Colecciones
+//
+//  Created by Naomi Solanch Veliz Pie on 2/09/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
