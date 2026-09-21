37a1c7806b2f0c4c35b5ee57d7467e4103080e16
Naomi Veliz <solanch.veliz@tecsup.edu.pe>
2026-09-20 19:54:56 -0500

    feat: conectar elementos del storyboard con ViewController

--- Semana05/ActividadInterfaces/ActividadInterfaces/ViewController.swift
+++ Semana05/ActividadInterfaces/ActividadInterfaces/ViewController.swift
@@ -8,9 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var capitalTextField:UITextField!
+    @IBOutlet weak var tasaTextField: UITextField!
+    @IBOutlet weak var plazoTextField: UITextField!
+    
+    
+    @IBOutlet weak var cuotaLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+       
     }
 
 
